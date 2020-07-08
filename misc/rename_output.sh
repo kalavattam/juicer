29998e9f0f32cd226ed2b5d3c31ee3b60b6411e6
Kris <kalavattam@gmail.com>
2020-07-08 16:30:06 -0400

    Debugging, tuning the commands.

--- misc/rename_output.sh
+++ misc/rename_output.sh
@@ -8,6 +8,6 @@ PARENT_DIR=$(dirname `pwd` | xargs basename)
 
 echo "The parent directory is ${PARENT_DIR}"
 
-find . -name '*' -type f -print0 | parallel -0 --keep-order echo "mv {/} ${PARENT_DIR}_{/}"
-find . -name '*' -type f -print0 | parallel -0 --keep-order mv {/} ${PARENT_DIR}_{/}
-parallel --keep-order 'echo -e "{1/}\t{2/}_{1/}"' ::: `find . -name '*' -type f` ::: `echo ${PARENT_DIR}` > `echo "${PARENT_DIR}_rename.txt"`
+parallel --keep-order 'echo -e "{1/}\t{2/}_{1/}"' ::: `find . -name '*' ! -name '*.sh' -type f` ::: `echo ${PARENT_DIR}` > `echo "${PARENT_DIR}_rename.txt"`
+find . -name '*' ! -name '*.sh' -type f -print0 | parallel -0 --keep-order echo "mv {/} ${PARENT_DIR}_{/}"
+find . -name '*' ! -name '*.sh' -type f -print0 | parallel -0 --keep-order mv {/} ${PARENT_DIR}_{/}
