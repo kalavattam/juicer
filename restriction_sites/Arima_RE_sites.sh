84692219a71652bc064915b775e7417bb29c609a
Kris <kalavattam@gmail.com>
2020-07-08 10:49:47 -0400

    Slight tweaks and adding chrom_size files without scaffold info (unlocalized, unplaced, etc.)

--- restriction_sites/Arima_RE_sites.sh
+++ restriction_sites/Arima_RE_sites.sh
@@ -12,7 +12,7 @@
 
 #  Set up variables for experiment name, directory
 SCRIPT_NAME=Arima_RE_sites.sh
-WORK_DIR="/data/namekawa_hpc/HiC_TBR/code_etc"  # Can change via hard coding
+WORK_PATH="/data/namekawa_hpc/HiC_TBR/code_etc"  # Can change via hard coding
          
 #  Load necessary CCHMC LSF module
 module load python3/3.7.1  # However, should work with Python 2.7 too
@@ -22,7 +22,7 @@ printf "${SCRIPT_NAME} run by `whoami` on `date` in\n`pwd`\n\n"
 printf "`python --version`"
 
 #  Call the ELA Lab code with appropriate arguments
-python ${WORK_DIR}"/juicer/misc/generate_site_positions.py" \
+python ${WORK_PATH}"/juicer/misc/generate_site_positions.py" \
 Arima \
 mm10 \
-${WORK_DIR}"/juicer/references/mm10.fa"
+${WORK_PATH}"/juicer/references/mm10.fa"
