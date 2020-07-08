4b12fc9ba2d2a375841f4648073f2b8333676eae
Kris <kalavattam@gmail.com>
2020-07-08 10:03:37 -0400

    Cleaning up the shell script, adding a gzipped RE site text file for quick, easy use in the future...

--- restriction_sites/Arima_RE_sites.sh
+++ restriction_sites/Arima_RE_sites.sh
@@ -10,12 +10,9 @@
 #BSUB -e Arima_RE_sites.sh.%J.err
 #BSUB -o Arima_RE_sites.sh.%J.out
 
-#  Run script in Bash "safe mode"
-# set -Eeuxo pipefail
-
-#  Set up variables for experiment name, LOG, directory
+#  Set up variables for experiment name, directory
 SCRIPT_NAME=Arima_RE_sites.sh
-WORKDIR="/data/namekawa_hpc/HiC_TBR/code_etc"  # Can change via hard coding
+WORK_DIR="/data/namekawa_hpc/HiC_TBR/code_etc"  # Can change via hard coding
          
 #  Load necessary CCHMC LSF module
 module load python3/3.7.1  # However, should work with Python 2.7 too
@@ -25,7 +22,7 @@ printf "${SCRIPT_NAME} run by `whoami` on `date` in\n`pwd`\n\n"
 printf "`python --version`"
 
 #  Call the ELA Lab code with appropriate arguments
-python ${WORKDIR}"/juicer/misc/generate_site_positions.py" \
+python ${WORK_DIR}"/juicer/misc/generate_site_positions.py" \
 Arima \
 mm10 \
-${WORKDIR}"/juicer/references/mm10.fa"
+${WORK_DIR}"/juicer/references/mm10.fa"
